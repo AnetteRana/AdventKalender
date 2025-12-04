736cf66c69c9ad514e956ff9f977c290591fbf8b
Anette Rana <pertrosfoliea@gmail.com>
2025-12-04 19:05:47 +0100

    levels checks how many houses it has on startup and set variable

--- Levels/level_script.gd
+++ Levels/level_script.gd
@@ -3,6 +3,14 @@ extends Node2D
 @export var Level_ID: String
 @export var HousesInLevel: int
 
-func _ready() -> void:
-	GameManager.currentLevelID = Level_ID
+func _setHouseNumber():
+	HousesInLevel = get_tree().get_nodes_in_group("house").size()
 	GameManager.currentHousesInLevel = HousesInLevel
+
+func _ready() -> void:
+	
+	_setHouseNumber()
+	
+	GameManager.currentLevelID = Level_ID
+	
+	
